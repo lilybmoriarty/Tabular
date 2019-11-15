4469ae8bb7479fd7cd61160af26211a782ae5b9a
Lillian Moriarty <lilybmoriarty@gmail.com>
2019-11-15 16:24:03 -0500

    added back code from before

--- tablist.js
+++ tablist.js
@@ -1,18 +1,18 @@
 // Given an array of URLs, build a DOM list of those URLs in the
 // browser action popup.
-// let allTabs = [];
+console.log("do i run??!?!!?!?!?!??!");
+
 chrome.tabs.query({ currentWindow: true }, function(tabs) {
-  allTabs = [...tabs];
+  let allTabs = [...tabs];
   buildOpenURLList("list-open-tabs", allTabs);
+  let groupedTabs = groupByDomain(allTabs);
+  openUniqueTabs(groupedTabs);
 });
 
 function buildOpenURLList(divName, list) {
-  console.log("this is divName", divName);
   let listDiv = document.getElementById(divName);
-  console.log("this is listDiv", listDiv);
 
   for (let i = 0; i < list.length; i++) {
-    console.log("this is list[i]", list[i]);
     let li = document.createElement("li");
     let a = document.createElement("a");
     a.href = list[i].url;
@@ -21,4 +21,36 @@ function buildOpenURLList(divName, list) {
     li.appendChild(a);
     listDiv.appendChild(li);
   }
+}
+
+// there are limitations to this - google sites sometimes start with or only include the keyword 'chrome' i.e. https://developer.chrome.com/ will have a different domain than https://mail.google.com even though they are both technically 'google'
+
+function groupByDomain(list) {
+  let listUrls = list.map(obj => obj.url);
+  let uniqueDomains = {};
+
+  listUrls
+    .map(url => {
+      return [url.split(".com")[0], url];
+    })
+    .forEach(domain => {
+      if (domain[0].includes(".google") || domain[0].startsWith("chrome")) {
+        return uniqueDomains[".google"]
+          ? uniqueDomains[".google"].push(domain[1])
+          : (uniqueDomains[".google"] = [domain[1]]);
+      } else
+        uniqueDomains[domain[0]]
+          ? uniqueDomains[domain[0]].push(domain[1])
+          : (uniqueDomains[domain[0]] = [domain[1]]);
+    });
+  return uniqueDomains;
+}
+
+function openUniqueTabs(obj) {
+  console.log("do i go in here?");
+  console.log("this is obj", obj);
+  // for (let key in obj) {
+  //   console.log("is this happening?");
+  //   chrome.tabs.create({ url: "/tablist.html", active: false });
+  // }
 }
